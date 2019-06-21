dad08e5c9694fb20f5b45f00ca99186efdfdf46d
Dmytro Panchenko <mcyber35@gmail.com>
2019-06-21 19:58:54 +0300

    Added test for parsing

---
+++ parse_test.go
@@ -0,0 +1,20 @@
+package asn1per
+
+import (
+	"fmt"
+	"math/big"
+	"testing"
+)
+
+type myTestData struct {
+	OrdinaryNumber int `asn1:"range(10..5),integer,something_else"`
+	Fixed64Number  int64
+	BigNumber      *big.Int
+}
+
+func TestStructParse(t *testing.T) {
+	var tS myTestData
+
+	tag, fParams := GetTagParams(tS)
+	fmt.Println(tag, fParams)
+}
