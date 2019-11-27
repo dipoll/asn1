8ed759fc67979e0215e5ec537054af05ab0a604c
dipoll <mcyber35@gmail.com>
2019-11-27 18:30:58 +0200

    Fixed failed build

--- spec/field.go
+++ spec/field.go
@@ -1,5 +1,11 @@
 package spec
 
+import (
+	"encoding/asn1"
+	"strconv"
+	"strings"
+)
+
 type FieldSpec struct {
 	Optional     bool   // true iff the field is OPTIONAL
 	Explicit     bool   // true iff an EXPLICIT tag is in use.
@@ -12,4 +18,60 @@ type FieldSpec struct {
 	Set          bool   // true iff this should be encoded as a SET
 	OmitEmpty    bool   // true iff this should be omitted if empty when marshaling.
 	Range        []IntRange
+}
+
+// ParseFieldParameters Given a tag string with the format specified in the package comment,
+// parseFieldParameters will parse it into a fieldParameters structure,
+// ignoring unknown parts of the string.
+func ParseFieldSpec(str string) (ret FieldSpec) {
+	for _, part := range strings.Split(str, ",") {
+		switch {
+		case part == "optional":
+			ret.Optional = true
+		case part == "explicit":
+			ret.Explicit = true
+			if ret.Tag == nil {
+				ret.Tag = new(int)
+			}
+		case part == "generalized":
+			ret.TimeType = asn1.TagGeneralizedTime
+		case part == "utc":
+			ret.TimeType = asn1.TagUTCTime
+		case part == "ia5":
+			ret.StringType = asn1.TagIA5String
+		case part == "printable":
+			ret.StringType = asn1.TagPrintableString
+		case part == "numeric":
+			ret.StringType = asn1.TagNumericString
+		case part == "utf8":
+			ret.StringType = asn1.TagUTF8String
+		case strings.HasPrefix(part, "default:"):
+			i, err := strconv.ParseInt(part[8:], 10, 64)
+			if err == nil {
+				ret.DefaultValue = new(int64)
+				*ret.DefaultValue = i
+			}
+		case strings.HasPrefix(part, "tag:"):
+			i, err := strconv.Atoi(part[4:])
+			if err == nil {
+				ret.Tag = new(int)
+				*ret.Tag = i
+			}
+		case part == "set":
+			ret.Set = true
+		case part == "application":
+			ret.Application = true
+			if ret.Tag == nil {
+				ret.Tag = new(int)
+			}
+		case part == "private":
+			ret.Private = true
+			if ret.Tag == nil {
+				ret.Tag = new(int)
+			}
+		case part == "omitempty":
+			ret.OmitEmpty = true
+		}
+	}
+	return
 }
